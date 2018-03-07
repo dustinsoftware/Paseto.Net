93372a01afea310ddd063400fac0730161bca978
Dustin Masters <dustin.masters@faithlife.com>
2018-03-07 15:13:11 -0800

    Cleanup

--- Paseto/Paseto.cs
+++ Paseto/Paseto.cs
@@ -21,15 +21,13 @@ namespace Paseto
 
 			string header = "v2.public.";
 
-			string m2 = PAE(new[] { header, payload, footer });
+			byte[] m2 = PreAuthEncode(new[] { header, payload, footer }.Select(Encoding.UTF8.GetBytes).ToArray());
 
 			var encryptAlgorithm = new Ed25519();
 			using (var key = Key.Import(encryptAlgorithm, _options.PrivateKey, KeyBlobFormat.RawPrivateKey))
 			{
-				var data = Encoding.UTF8.GetBytes(m2);
-
 				string footerToAppend = footer == "" ? "" : $".{ToBase64Url(Encoding.UTF8.GetBytes(footer))}";
-				return $"{header}{ToBase64Url(Encoding.UTF8.GetBytes(payload).Concat(encryptAlgorithm.Sign(key, data)))}{footerToAppend}";
+				return $"{header}{ToBase64Url(Encoding.UTF8.GetBytes(payload).Concat(encryptAlgorithm.Sign(key, m2)))}{footerToAppend}";
 			}
 		}
 
@@ -41,24 +39,24 @@ namespace Paseto
 			const string header = "v2.public.";
 			Assert(signedMessage.StartsWith(header), "Token did not start with v2.public.");
 			var tokenParts = signedMessage.Split('.');
-			string footer = Encoding.UTF8.GetString(FromBase64Url(tokenParts.Length > 3 ? tokenParts[3] : ""));
+			byte[] footer = FromBase64Url(tokenParts.Length > 3 ? tokenParts[3] : "");
 
 			var bytes = FromBase64Url(tokenParts[2]);
 			Assert(bytes.Length >= 64, "Token was less than 64 bytes long");
 			byte[] signature = bytes.Skip(bytes.Length - 64).ToArray();
 			byte[] payload = bytes.Take(bytes.Length - 64).ToArray();
 
-			string m2 = PAE(new[] { header, Encoding.UTF8.GetString(payload), footer });
+			byte[] m2 = PreAuthEncode(new[] { Encoding.UTF8.GetBytes(header), payload, footer });
 
 			var encryptAlgorithm = new Ed25519();
 
 			var publicKey = PublicKey.Import(encryptAlgorithm, _options.PublicKey, KeyBlobFormat.RawPublicKey);
-			encryptAlgorithm.Verify(publicKey, Encoding.UTF8.GetBytes(m2), signature);
+			encryptAlgorithm.Verify(publicKey, m2, signature);
 
 			return new ParsedPaseto
 			{
 				Payload = Encoding.UTF8.GetString(payload),
-				Footer = footer,
+				Footer = Encoding.UTF8.GetString(footer),
 			};
 		}
 
@@ -71,30 +69,30 @@ namespace Paseto
 		}
 
 		// https://github.com/paragonie/paseto/blob/785723a02bc27e0e90821b0852d9e86573bbe63d/docs/01-Protocol-Versions/Common.md#authentication-padding
-		public static string PAE(IReadOnlyList<string> pieces)
+		public static byte[] PreAuthEncode(IReadOnlyList<byte[]> pieces)
 		{
-			string output = LE64(pieces.Count);
-			foreach (string piece in pieces)
+			byte[] output = ConvertToLittleEndian(pieces.Count);
+			foreach (byte[] piece in pieces)
 			{
-				output += LE64(piece.Length);
-				output += piece;
+				output = output.Concat(ConvertToLittleEndian(piece.Length)).ToArray();
+				output = output.Concat(piece).ToArray();
 			}
 			return output;
 		}
 
-		public static string LE64(int source)
+		public static byte[] ConvertToLittleEndian(int source)
 		{
-			string str = "";
+			byte[] output = new byte[0];
 			for (int i = 0; i < 8; i++)
 			{
-				str += Encoding.ASCII.GetString(new[] { (byte) (source & 255) });
+				output = output.Concat(new[] { (byte)(source & 255) }).ToArray();
 				source = source >> 8;
 			}
-			return str;
+			return output;
 		}
 
 		public static string ToBase64Url(IEnumerable<byte> source) => Convert.ToBase64String(source.ToArray())
-			.Replace("=","")
+			.Replace("=", "")
 			.Replace('+', '-')
 			.Replace('/', '_');
 
